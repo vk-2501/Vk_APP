929abfc88a0574fb9c5a4b0cf31b0565bc564bc0
Jaskeerat Singh <jaskeerat10000@gmail.com>
2021-12-22 13:58:56 +0530

    made changes in foodrow , added footer , changes in checkout , mainImage.css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./css/Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+            <div>
+                Hello AlanEats footer
+            </div>
+        </div>
+    )
+}
+
+export default Footer
